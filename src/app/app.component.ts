95663d266dc05e1d3a52a185ce5d19377733fb99
Reem <Reem@DESKTOP-OQO8JHP>
2024-11-23 22:00:31 +0200

    add new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularObservables';
+  data:any[] = [];
+
+  //create an observanle
+  myObservable = new Observable((observer)=>{
+
+  });
 }
